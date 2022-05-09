cce75ef176b26536fddf3f93cedfdd5b345e92c6
Ufuk Güleç <oug505@gmail.com>
2022-05-09 19:26:02 +0300

    Ürün Listeleme sayfası ve sınıfları oluşturuldu.

---
+++ BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product entity)
+        {
+            _productDal.Add(entity);
+        }
+
+        public void Delete(Product entity)
+        {
+            _productDal.Delete(entity);
+        }
+
+        public Product Get(int id)
+        {
+            return _productDal.Get(id);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public void Update(Product entity)
+        {
+            _productDal.Update(entity);
+        }
+    }
+}
---
+++ Rys/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rys.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager productManager = new ProductManager(new EfProductRepository());
+        public IActionResult Index()
+        {
+            var values = productManager.GetAll();
+            return View(values);
+        }
+    }
+}
