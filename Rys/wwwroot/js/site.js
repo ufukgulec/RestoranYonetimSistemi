bc51915260b507cb415454b1eeb5a090047794a6
Ufuk Güleç <oug505@gmail.com>
2022-05-14 15:09:29 +0300

    Site teması üzerinde düzenlemeler yapıldı.

--- Rys/wwwroot/js/site.js
+++ Rys/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    ActiveNav();
+});
 
-// Write your JavaScript code.
+function ActiveNav() {
+    if (window.location.pathname.length > 1) {
+        var navs = document.getElementsByClassName("nav-link");
+        for (var i = 0; i < navs.length; i++) {
+            navs[i].classList.remove("active");
+        }
+        var selecetedMenu = window.location.pathname.split("/")[1];
+        var nav = document.getElementsByClassName("nav" + selecetedMenu)[0].classList.add("active");
+    }
+    else {
+        document.getElementById("homenavlink").classList.add("active");
+    }
+}
