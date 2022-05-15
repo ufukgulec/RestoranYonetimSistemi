73c194fafc07fa8bbf1045ec50ff3933fa87228e
Ufuk Güleç <oug505@gmail.com>
2022-05-15 20:50:56 +0300

    Telefon siparişleri için sınıflar ve index sayfası oluşturuldu.

---
+++ BusinessLayer/Concrete/PhoneOrderManager.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class PhoneOrderManager : IPhoneOrderService
+    {
+        IPhoneOrderDal _phoneOrderDal;
+        public PhoneOrderManager(IPhoneOrderDal phoneOrderDal)
+        {
+            _phoneOrderDal = phoneOrderDal;
+        }
+
+        public void Add(PhoneOrder entity)
+        {
+            _phoneOrderDal.Add(entity);
+        }
+
+        public void Delete(PhoneOrder entity)
+        {
+            _phoneOrderDal.Delete(entity);
+        }
+
+        public PhoneOrder Get(int id)
+        {
+            return _phoneOrderDal.Get(id);
+        }
+
+        public List<PhoneOrder> GetAll()
+        {
+            return _phoneOrderDal.GetAll();
+        }
+
+        public List<PhoneOrder> GetAll(string TableName)
+        {
+            return _phoneOrderDal.GetAll(TableName);
+        }
+
+        public void Update(PhoneOrder entity)
+        {
+            _phoneOrderDal.Update(entity);
+        }
+    }
+}
---
+++ Rys/Controllers/PhoneOrderController.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rys.Controllers
+{
+    public class PhoneOrderController : Controller
+    {
+        PhoneOrderManager orderManager = new PhoneOrderManager(new EfPhoneOrderRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
