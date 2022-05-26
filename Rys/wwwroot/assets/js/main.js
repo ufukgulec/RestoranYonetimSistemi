32afc184f329ac08d1e3a1c7bf55b9895430b7df
Ufuk Güleç <oug505@gmail.com>
2022-05-26 21:06:26 +0300

    Temanın menüsünde aktif class'ı kullanmak için jquery kullanıldı js yazıldı.

--- Rys/wwwroot/assets/js/main.js
+++ Rys/wwwroot/assets/js/main.js
@@ -2,21 +2,7 @@
  * Main
  */
 'use strict';
-ActiveNav();
-function ActiveNav() {
-    if (window.location.pathname.length > 1) {
-        var navs = document.getElementsByClassName("menu-item");
-        for (var i = 0; i < navs.length; i++) {
-            navs[i].classList.remove("active");
-        }
-        var selecetedMenu = window.location.pathname.split("/")[1];
-        var nav = document.getElementsByClassName("nav" + selecetedMenu)[0].classList.add("active");
-    }
 
-    else {
-        document.getElementById("homenavlink").classList.add("active");
-    }
-}
 let menu, animate;
 
 (function () {
--- Rys/wwwroot/js/site.js
+++ Rys/wwwroot/js/site.js
@@ -4,14 +4,14 @@
 
 function ActiveNav() {
     if (window.location.pathname.length > 1) {
-        var navs = document.getElementsByClassName("nav-link");
+        var navs = document.getElementsByClassName("menu-item");
         for (var i = 0; i < navs.length; i++) {
             navs[i].classList.remove("active");
         }
         var selecetedMenu = window.location.pathname.split("/")[1];
-        var nav = document.getElementsByClassName("nav" + selecetedMenu)[0].classList.add("active");
+        document.getElementsByClassName("nav" + selecetedMenu)[0].classList.add("active");
     }
-    
+
     else {
         document.getElementById("homenavlink").classList.add("active");
     }
