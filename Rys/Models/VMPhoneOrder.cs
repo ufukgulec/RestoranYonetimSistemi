99608628b1a7e5a291f95fddf82a104ff7df4305
Ufuk Güleç <oug505@gmail.com>
2022-06-01 23:00:39 +0300

    Sunum katmanında telefon sipariş sınıfı düzenlendi.

--- Rys/Models/VMPhoneOrder.cs
+++ Rys/Models/VMPhoneOrder.cs
@@ -7,23 +7,26 @@ namespace Rys.Models
 {
     public class VMPhoneOrder : EntityLayer.Concrete.PhoneOrder
     {
-        PhoneOrderManager orderManager = new PhoneOrderManager(new EfOrderRepository<PhoneOrder>());
+        private PhoneOrderManager orderManager = new PhoneOrderManager(new EfOrderRepository<PhoneOrder>());
+        private RegionManager<District> districtRegionManager = new RegionManager<District>(new EfGenericRepository<District>());
+        private RegionManager<Street> streetRegionManager = new RegionManager<Street>(new EfGenericRepository<Street>());
+        private GenericManager<PhoneOrderDetail> detailsManager = new GenericManager<PhoneOrderDetail>(new EfGenericRepository<PhoneOrderDetail>());
+        private GenericManager<Customer> customerManager = new GenericManager<Customer>(new EfGenericRepository<Customer>());
+        private ProductManager productManager = new ProductManager(new EfProductRepository());
 
-        RegionManager<District> districtRegionManager = new RegionManager<District>(new EfGenericRepository<District>());
-        RegionManager<Street> streetRegionManager = new RegionManager<Street>(new EfGenericRepository<Street>());
-
-        GenericManager<PhoneOrderDetail> detailsManager = new GenericManager<PhoneOrderDetail>(new EfGenericRepository<PhoneOrderDetail>());
-
-        ProductManager productManager = new ProductManager(new EfProductRepository());
-
-        public List<Product> products { get; set; }
 
         public VMPhoneOrder(PhoneOrder phoneOrder)
         {
             Id = phoneOrder.Id;
             Status = phoneOrder.Status;
             OrderTime = phoneOrder.OrderTime;
+            CustomerId = phoneOrder.CustomerId;
+
+            Customer = customerManager.Get(CustomerId);
+            Customer.Street = streetRegionManager.Get(Customer.StreetId);
+            Customer.Street.District = districtRegionManager.Get(Customer.Street.DistrictId);
             PhoneOrderDetails = detailsManager.GetAll(x => x.PhoneOrderId == Id);
+
             foreach (var item in PhoneOrderDetails)
             {
                 item.Product = productManager.Get(item.ProductId);
