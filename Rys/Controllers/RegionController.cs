c29b393dde42150a0771df32b36384ce30657051
Ufuk Güleç <oug505@gmail.com>
2022-06-05 00:20:53 +0300

    Bölgeler için mahalle ve sokak listeleme sayfasında değişiklik yapıldı ve ekleme sayfası oluşturuldu.

--- Rys/Controllers/RegionController.cs
+++ Rys/Controllers/RegionController.cs
@@ -12,27 +12,34 @@ namespace Rys.Controllers
     {
         RegionManager<District> districtRegionManager = new RegionManager<District>(new EfGenericRepository<District>());
         RegionManager<Street> streetRegionManager = new RegionManager<Street>(new EfGenericRepository<Street>());
-        public IActionResult Index(string searchDistrict, string searchStreet)
+        public IActionResult Index(int? districtId)
         {
             VMRegion regionModel = new VMRegion();
-            if (!string.IsNullOrEmpty(searchDistrict))
-            {
-                if (regionModel.districts.Where(d => d.Name.ToLower(new CultureInfo("en-US", false)).Contains(searchDistrict.ToLower(new CultureInfo("en-US", false)))).ToList().Count > 0)
-                {
-                    District district = regionModel.districts.Where(d => d.Name.ToLower(new CultureInfo("en-US", false)).Contains(searchDistrict.ToLower())).First();
-                    regionModel.streets = streetRegionManager.GetAll(d => d.DistrictId == district.Id).ToList();
-                }
 
-            }
-            else if (!string.IsNullOrEmpty(searchStreet))
+            if (districtId != null)
             {
-                if (regionModel.streets.Where(d => d.Name.ToLower(new CultureInfo("en-US", false)).Contains(searchStreet.ToLower(new CultureInfo("en-US", false)))).ToList().Count > 0)
-                {
-                    regionModel.streets = regionModel.streets.Where(d => d.Name.ToLower(new CultureInfo("en-US", false)).Contains(searchStreet.ToLower())).ToList();
-                }
+                regionModel.streets = regionModel.streets.Where(x => x.DistrictId == districtId).ToList();
+
             }
 
             return View(regionModel);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(districtRegionManager.GetAll());
+        }
+        [HttpPost]
+        public IActionResult AddDistrict(District district)
+        {
+            districtRegionManager.Add(district);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult AddStreet(Street street)
+        {
+            streetRegionManager.Add(street);
+            return RedirectToAction("Index");
         }
     }
 }
