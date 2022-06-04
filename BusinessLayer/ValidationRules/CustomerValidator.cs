f069ecb6232350675ea6943bb21054d9fb1271fb
Ufuk Güleç <oug505@gmail.com>
2022-06-04 23:26:12 +0300

    FluentValidation ile müşteri ekleme sayfasında kontrolleri sağlandı.

---
+++ BusinessLayer/ValidationRules/CustomerValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Telefon numarası boş geçilemez.");
+            RuleFor(x => x.PhoneNumber).MaximumLength(10).MinimumLength(10).Matches(new Regex("\\d")).WithMessage("Geçerli telefon numarası giriniz.");
+            RuleFor(x => x.StreetId).NotEmpty().WithMessage("Ürün kategorisi seçiniz.");
+        }
+    }
+}
--- Rys/Controllers/CustomerController.cs
+++ Rys/Controllers/CustomerController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Rys.Models;
 
@@ -9,6 +11,7 @@ namespace Rys.Controllers
     public class CustomerController : Controller
     {
         CustomerManager customerManager = new CustomerManager(new EfCustomerRepository());
+        VMRegion regionModel = new VMRegion();
         public IActionResult Index()
         {
             var values = customerManager.GetAll("Street", "Street.District");
@@ -17,7 +20,7 @@ namespace Rys.Controllers
         [HttpGet]
         public IActionResult Add()
         {
-            VMRegion regionModel = new VMRegion();
+
             ViewData["Streets"] = regionModel.streets;
 
             return View();
@@ -25,8 +28,23 @@ namespace Rys.Controllers
         [HttpPost]
         public IActionResult Add(Customer customer)
         {
-            customerManager.Add(customer);
-            return RedirectToAction("Index");
+            CustomerValidator validationRules = new CustomerValidator();
+            ValidationResult validationResult = validationRules.Validate(customer);
+            if (validationResult.IsValid)
+            {
+                customerManager.Add(customer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                ViewData["Streets"] = regionModel.streets;
+                return View();
+            }
+
         }
         public IActionResult Delete(int id)
         {
