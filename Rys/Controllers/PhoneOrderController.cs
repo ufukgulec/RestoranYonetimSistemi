55072e745af019071bb611fe3ab7f68f22d57e51
Ufuk Güleç <oug505@gmail.com>
2022-06-05 12:41:32 +0300

    Telefon siparişi alırken müşterinin kontrolü sağlandı kayıtlı değilse veritabanına kaydedecek metot yazıldı.

--- Rys/Controllers/PhoneOrderController.cs
+++ Rys/Controllers/PhoneOrderController.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Rys.Models;
+using System;
 using System.Linq;
 
 namespace Rys.Controllers
@@ -33,12 +34,52 @@ namespace Rys.Controllers
 
             return View(vm);
         }
-        public IActionResult Add() // Sipariş tablosu için telefon, Sokak id alınması lazım sipariş detay için ürün id ve adeti alınması lazım
+        [HttpGet]
+        public IActionResult Add(int CustomerId) // Sipariş tablosu için telefon, Sokak id alınması lazım sipariş detay için ürün id ve adeti alınması lazım
         {
             //PhoneOrder tablosuna ekleme yapılacak sonra PhoneOrderDetails tablosuna PhoneOrder id ile ürün eklemesi yapılacak.
             ViewData["Phones"] = VMPhones.GetAll();
+            ViewData["Streets"] = new VMRegion().streets;
 
             return View(orderManager.GetAll());
+        }
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult CustomerControl(string phoneNo)
+        {
+            //Ekli müşteri mi ?
+            ViewData["Phones"] = VMPhones.GetAll();
+            ViewData["Streets"] = null;
+            if (!String.IsNullOrEmpty(phoneNo))
+            {
+                CustomerManager customerManager = new CustomerManager(new EfCustomerRepository());
+                var values = customerManager.GetAll(x => x.PhoneNumber == phoneNo);
+                if (values.Count == 0)
+                {
+                    ViewData["Streets"] = new VMRegion().streets;
+                    return View(new Customer { PhoneNumber = phoneNo });
+                }
+                else
+                {
+                    return RedirectToAction("Add", values.First().Id);//<--
+                }
+            }
+            else
+            {
+                return View();
+            }
+
+
+        }
+        public IActionResult CustomerAdd(Customer customer)
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerRepository());
+            customerManager.Add(customer);
+            int CustomerId = customerManager.GetAll().Last().Id;
+            return RedirectToAction("Add", CustomerId);
         }
     }
 }
