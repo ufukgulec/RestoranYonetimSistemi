accb20e4465fb3ae72f8217f7ccce1a6c2ecd332
Ufuk Güleç <oug505@gmail.com>
2022-06-07 20:26:18 +0300

    Telefon siparişinde ürün seçme ve adet girilmesi için bir sayaç yapıldı.

--- Rys/wwwroot/js/site.js
+++ Rys/wwwroot/js/site.js
@@ -16,3 +16,50 @@ function ActiveNav() {
         document.getElementById("homenavlink").classList.add("active");
     }
 }
+const buttons = document.querySelectorAll(".btn-counter");
+const minValue = 0;
+const maxValue = 10;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        // 1. Get the clicked element
+        const element = event.currentTarget;
+        // 2. Get the parent
+        const parent = element.parentNode;
+        // 3. Get the number (within the parent)
+        const numberContainer = parent.querySelector(".number-product");
+        const number = parseFloat(numberContainer.textContent);
+        // 4. Get the minus and plus buttons
+        const increment = parent.querySelector(".plus");
+        const decrement = parent.querySelector(".minus");
+        // 5. Change the number based on click (either plus or minus)
+        const newNumber = element.classList.contains("plus")
+            ? number + 1
+            : number - 1;
+
+        numberContainer.textContent = newNumber;
+        console.log(newNumber);
+
+        // 6. Disable and enable buttons based on number value (and undim number)
+        if (newNumber === minValue) {
+            decrement.disabled = true;
+            numberContainer.classList.add("dim");
+            // Make sure the button won't get stuck in active state (Safari)
+            element.blur();
+        } else if (newNumber > minValue && newNumber < maxValue) {
+            decrement.disabled = false;
+            increment.disabled = false;
+            numberContainer.classList.remove("dim");
+        } else if (newNumber === maxValue) {
+            increment.disabled = true;
+            numberContainer.textContent = `${newNumber}+`;
+            element.blur();
+        }
+
+        if (element.classList.contains("plus")) {
+            alert("Ekle")
+        } else {
+            alert("Sil")
+        }
+    });
+});
