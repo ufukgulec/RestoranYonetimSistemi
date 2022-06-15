20dd1d85c918a2de585b2a27d9d4d8dba4b1e282
Ufuk Güleç <oug505@gmail.com>
2022-06-15 20:32:05 +0300

    Masa siparişi için sınıflar oluşturuldu.

---
+++ BusinessLayer/Concrete/TableOrderManager.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class TableOrderManager : IOrderService<TableOrder>
+    {
+        IOrderDal<TableOrder> _tableOrderDal;
+        public TableOrderManager(IOrderDal<TableOrder> tableOrderDal)
+        {
+            _tableOrderDal = tableOrderDal;
+        }
+
+        public void Add(TableOrder entity)
+        {
+            _tableOrderDal.Add(entity);
+        }
+
+        public void Delete(TableOrder entity)
+        {
+            _tableOrderDal.Delete(entity);
+        }
+
+        public TableOrder Get(int id)
+        {
+            return _tableOrderDal.Get(id);
+        }
+
+        public TableOrder Get(Expression<Func<TableOrder, bool>> Expression)
+        {
+            return _tableOrderDal.Get(Expression);
+        }
+
+        public List<TableOrder> GetAll()
+        {
+            return _tableOrderDal.GetAll();
+        }
+
+        public List<TableOrder> GetAll(string TableName)
+        {
+            return _tableOrderDal.GetAll(TableName);
+        }
+
+        public List<TableOrder> GetAll(Expression<Func<TableOrder, bool>> Expression)
+        {
+            return _tableOrderDal.GetAll(Expression);
+        }
+
+        public void Update(TableOrder entity)
+        {
+            _tableOrderDal.Update(entity);
+        }
+    }
+}
---
+++ Rys/Controllers/TableOrderController.cs
@@ -0,0 +1,82 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Rys.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rys.Controllers
+{
+    public class TableOrderController : Controller
+    {
+        #region Managers
+        TableOrderManager orderManager = new TableOrderManager(new EfOrderRepository<TableOrder>());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        CustomerManager customerManager = new CustomerManager(new EfCustomerRepository());
+        GenericManager<TableOrderDetail> DetailManager = new GenericManager<TableOrderDetail>(new EfGenericRepository<TableOrderDetail>());
+        #endregion
+        public IActionResult Index()
+        {
+            var values = orderManager.GetAll("Customer").Where(x => x.OrderTime.Date == System.DateTime.Now.Date).ToList();//Günlük siparişler
+            return View(values);
+        }
+        public IActionResult History()
+        {
+            var values = orderManager.GetAll("Customer");//Tüm Siparişler
+            return View(values);
+        }
+        public IActionResult Complete(int id)
+        {
+            var order = orderManager.Get(id);
+            order.Status = false;
+            orderManager.Update(order);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            // VMPhoneOrder vm = new VMPhoneOrder(orderManager.Get(id));
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add(int id) // Sipariş tablosu için telefon, Sokak id alınması lazım sipariş detay için ürün id ve adeti alınması lazım
+        {
+            var currrentCustomer = VMCustomer.Get(id);
+            ViewData["Customer"] = currrentCustomer;
+
+            return View(categoryManager.GetAll("Products").Where(x => x.Status).Where(x => x.Products.Count != 0).ToList());
+        }
+        [HttpPost]
+        public IActionResult Add(IEnumerable<VMBucket> BucketList, int currentCustomer)
+        {
+            //PhoneOrder newPhoneOrder = new PhoneOrder()
+            //{
+            //    CustomerId = currentCustomer,
+            //    OrderTime = DateTime.Now,
+            //    Status = true,
+
+            //};
+            //orderManager.Add(newPhoneOrder);
+
+            int currentOrderId = orderManager.GetAll().Last().Id;
+            OrderDetailAdd(currentOrderId, BucketList);
+            return RedirectToAction("Index", "PhoneOrder");
+        }
+
+        private void OrderDetailAdd(int currentOrderId, IEnumerable<VMBucket> BucketList)
+        {
+            foreach (var item in BucketList)
+            {
+                TableOrderDetail detail = new TableOrderDetail()
+                {
+                    TableOrderId = currentOrderId,
+                    ProductId = item.ProductId,
+                    ProductCount = item.ProductCount
+                };
+                DetailManager.Add(detail);
+            }
+        }
+    }
+}
